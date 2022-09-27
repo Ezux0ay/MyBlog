98e427f7d2a7629aa60fb031394f0cc8551f50b9
Ezuxoay <1028833894@qq.com>
2022-09-27 16:36:17 +0800

    Initial commit

---
+++ src/test/java/top/naccl/BlogApiApplicationTests.java
@@ -0,0 +1,18 @@
+package top.naccl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		String zwwoaini;
+	}
+
+	@Test
+	void test() {
+	}
+
+}
